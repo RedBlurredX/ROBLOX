06cca210524af8a7d3fa176d59e691fa5280ad88
RedBlurredX <82548628+RedBlurredX@users.noreply.github.com>
2021-04-15 18:40:01 -0300

    Create MineAllOres.lua

---
+++ Games/EmitaCityRP/AutoFarm/MineAllOres.lua
@@ -0,0 +1,10 @@
+for i = 1, 4 do
+wait()
+for i, v in pairs(workspace.Mines:GetChildren()) do
+local args = {
+    [1] = v,
+    [2] = "Tier6 Pickaxe"
+}
+game:GetService("ReplicatedStorage").Remotes.Shop.MineEvent:FireServer(unpack(args))
+end
+end
