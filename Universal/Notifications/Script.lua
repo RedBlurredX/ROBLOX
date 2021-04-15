3c227250b232801ec56f60d11de1d3f71097acae
RedBlurredX <82548628+RedBlurredX@users.noreply.github.com>
2021-04-15 17:45:38 -0300

    Update Script.lua

--- Universal/Notifications/Script.lua
+++ Universal/Notifications/Script.lua
@@ -4,9 +4,9 @@ local NotificationTheme = {
 	MainColour = Color3.fromRGB(255, 119, 244),
 	BackgroundColour = Color3.fromRGB(25, 25, 25),
 	SecondaryBackgroundColour = Color3.fromRGB(15, 15, 15),
-	Success = Color3.fromRGB(25, 25, 25),
-	Error = Color3.fromRGB(25, 25, 25),
-	Alert = Color3.fromRGB(25, 25, 25),
+	Success = Color3.fromRGB(256, 256, 256),
+	Error = Color3.fromRGB(0, 0, 0),
+	Alert = Color3.fromRGB(255, 0, 0),
 	
 	-- // Text Configuration
 	BodyFont = Enum.Font.SourceSans,
