a98201e84981490ae667d1f03aa3e07bea2f5a02
RedBlurredX <82548628+RedBlurredX@users.noreply.github.com>
2021-05-02 16:40:13 -0300

    Made by: TrillyX

---
+++ ScriptDevelopment/Ui Library/NFlib/NotificationLib Loadstring.lua
@@ -0,0 +1,3 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/RedBlurredX/ROBLOX/main/ScriptDevelopment/Ui%20Library/NFlib/NotificationLib%20Source.lua"))()
+
+Library:Notification("Preview", "This is a preview of a notification using NFLib", 5, Color3.fromRGB(255, 255, 255))
